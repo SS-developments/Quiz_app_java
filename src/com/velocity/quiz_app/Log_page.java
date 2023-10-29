ccf1fa92cc172219dd031c75876d8bc4a4eb690d
SS-developments <swapnilawad2000@gmail.com>
2023-10-29 18:12:08 +0530

    login page added

--- src/com/velocity/quiz_app/Log_page.java
+++ src/com/velocity/quiz_app/Log_page.java
@@ -5,12 +5,16 @@ public class Log_page {
 	
 	public static void main (String args[]) {
 		
+		// different  objects for different class to access methods 
+		Login_page Login_page_obj = new Login_page();
+		
 		Scanner scan = new Scanner(System.in);
 		
 		int log_input;
 	
-		System.out.println ("Welcome ");
-		do {
+		System.out.println ("\t\t\t\tWelcome\n ");
+		
+		do {		    // adding do-while for getting correct input from user 
 		
 		System.out.println(" plase select \n 1.login \n 2.signup");
 		
@@ -19,12 +23,13 @@ public class Log_page {
 	}while(log_input==3);
 		
 		
+		
 		switch (log_input) {
 		
 		
 		case 1: {
+			Login_page_obj.login();
 			
-			System.out.println(" login ");
 		}
 			break;
 		case 2:{
---
+++ src/com/velocity/quiz_app/Login_page.java
@@ -0,0 +1,50 @@
+package com.velocity.quiz_app;
+
+import java.util.Scanner;
+
+public class Login_page {
+	
+//	
+//	public static void main(String[] args) {
+//		
+//		login();
+//	}
+	
+	
+	public   void login() {
+		Scanner login_sc = new Scanner (System.in);
+		int  login_i;
+		
+		do {
+		System.out.println("\nselct login \n 1.Student \n 2.Admin");
+		  login_i =  login_sc.nextInt();
+		}while(login_i>2);
+		
+		switch (login_i) {
+		
+		
+		case 1: {
+			
+			System.out.println("\n\twelcome back student\n \n plase enter user name:");
+//			String u_name= login_sc.next();
+			
+			System.out.println("login success");
+		}
+		break;
+		
+		
+		case 2: {
+			System.out.println("admin");
+			System.out.println("Hello Admin \n user name:Admin");
+			System.out.println(" plase enter password:");
+//			String u_name= login_sc.next();
+		}
+		break;
+		
+		
+		}
+		
+	}
+	
+	
+}
